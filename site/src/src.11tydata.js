dc070c017683d022525142f1b071aaf04539e4b6
Philippe Faist <phfaist@gmail.com>
2023-05-13 16:16:12 +0200

    citation in about page

--- site/src/src.11tydata.js
+++ site/src/src.11tydata.js
@@ -2,4 +2,6 @@ module.exports = {
     layout: "base_page",
     date: "Git last modified",
     permalink: (data) => `${ data.page.filePathStem }.${ data.page.outputFileExtension }`,
+
+    get_current_year: (data) => (new Date().getFullYear())
 };
