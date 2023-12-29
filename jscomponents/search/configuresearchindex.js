9fcc0f0ecf673770d150ce270344ebfa38c08f3d
Philippe Faist <phfaist@gmail.com>
2023-12-29 01:41:30 +0100

    unsaved file\

--- jscomponents/search/configuresearchindex.js
+++ jscomponents/search/configuresearchindex.js
@@ -1,11 +1,4 @@
-import { getLunrCustomOptionsAdvancedSetup } from '@phfaist/zoodbtools_search/lunradvancedsetup';
 
 export const lunrAdvancedOptions = {
     includeNGramsUpTo: 3,
 };
-
-export function configureSearchIndex(search_index)
-{
-    const indexLunrCustomOptions = getLunrCustomOptionsAdvancedSetup(lunrAdvancedOptions);
-    search_index.install_lunr_customization(indexLunrCustomOptions);
-}
