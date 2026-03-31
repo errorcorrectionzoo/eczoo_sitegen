f7be8c9e6a49fb145869df21fa2238b0d60e5de1
Philippe Faist <phfaist@gmail.com>
2026-04-01 01:35:46 +0200

    changed 41ai thingy; separate page. nicer.

--- jscomponents/aizoo20260401/setup.js
+++ jscomponents/aizoo20260401/setup.js
@@ -1,11 +1,11 @@
 
 import './AiZoo20260401.css';
-import eczooMainLogoUrl from 'url:../../site/static/icons/eczoo-main-logo-contrast.svg';
+//import eczooMainLogoUrl from 'url:../../site/static/icons/eczoo-main-logo-contrast.svg';
 
-import markdownit from 'markdown-it'
-import aiUpdateTextArticleMd from 'bundle-text:./ai-update-text-article.md';
+//import markdownit from 'markdown-it'
+//import aiUpdateTextArticleMd from 'bundle-text:./ai-update-text-article.md';
 
-function animateAiZoo20260401()
+function animateAiZoo20260401_banner()
 {
     let today = new Date();
     if (today.getDate() != 1 || today.getMonth() != 3) { // April is 3 since Jan is 0
@@ -17,65 +17,78 @@ function animateAiZoo20260401()
     bannerElement.innerHTML = `
 <div class="AiZoo20260401-BannerContent">
 <b>Important Error Correction Zoo Update: A new start with AI!</b>
-<a href="" class="AiZoo20260401-Banner-LearnMoreLink">Learn more...</a>
+<a href="/__41ai" class="AiZoo20260401-Banner-LearnMoreLink">Learn more...</a>
 </div>
 `;
 
-    const md = markdownit({
-        html: true,
-        linkify: true,
-        typographer: true
-    });
-    console.log(md);
-    const aiUpdateTextArticleHtml = md.render(aiUpdateTextArticleMd);
+    // let aEl = bannerElement.querySelector('a.AiZoo20260401-Banner-LearnMoreLink');
+    // aEl.addEventListener('click', (e) => {
+    //     console.log('Showing full AI announcement message...');
+    //     msgElement.classList.add('AiZoo20260401MsgArticleContainer-shown');
+    //     e.preventDefault();
+    //     return false;
+    // });
 
-    let msgElement = document.createElement('div');
-    msgElement.classList.add('AiZoo20260401MsgArticleContainer');
-    msgElement.innerHTML = `
-<div class="AiZoo20260401-MsgArticleWrap">
-<button class="AiZoo20260401-CloseBtn AiZoo20260401-CloseClickable" aria-label="Close">&times;</button>
-<div class="AiZoo20260401-MsgArticleContent">
-<div class="AiZoo20260401-MsgArticleContent-Title-Logo">
-<div class="AiZoo20260401-MsgArticleContent-Title">
-The AI Error Correction Zoo<!--: A new start with AI <span class="AiZoo20260401-wow-ai">AI</span>-->
-</div>
-<!--<img src="${eczooMainLogoUrl}">-->
-</div>
-<div class="AiZoo20260401-MsgArticleContent-MainContent">
-${aiUpdateTextArticleHtml}
-</div>
-<!--<a href="#" class="AiZoo20260401-CloseClickable">Close</a>-->
-</div>
-</div>
-`;
+    document.body.appendChild(bannerElement);
+}
 
-    msgElement.querySelector('.AiZoo20260401-CloseClickable').addEventListener('click', (e) => {
-        console.log('Close click!');
-        msgElement.classList.remove('AiZoo20260401MsgArticleContainer-shown');
-        e.preventDefault();
-        return false;
-    });
+function animateAiZoo20260401_aipostpage()
+{
+    // const md = markdownit({
+    //     html: true,
+    //     linkify: true,
+    //     typographer: true
+    // });
+    // console.log(md);
+    // const aiUpdateTextArticleHtml = md.render(aiUpdateTextArticleMd);
 
-    let titleEl = msgElement.querySelector('.AiZoo20260401-MsgArticleContent-Title');
-    msgElement.addEventListener('mousemove', (e) => {
+//     let msgElement = document.createElement('div');
+//     msgElement.classList.add('AiZoo20260401MsgArticleContainer');
+//     msgElement.innerHTML = `
+// <div class="AiZoo20260401-MsgArticleWrap">
+// <button class="AiZoo20260401-CloseBtn AiZoo20260401-CloseClickable" aria-label="Close">&times;</button>
+// <div class="AiZoo20260401-MsgArticleContent">
+// <div class="AiZoo20260401-MsgArticleContent-Title-Logo">
+// <div class="AiZoo20260401-MsgArticleContent-Title">
+// The AI Error Correction Zoo<!--: A new start with AI <span class="AiZoo20260401-wow-ai">AI</span>-->
+// </div>
+// <!--<img src="${eczooMainLogoUrl}">-->
+// </div>
+// <div class="AiZoo20260401-MsgArticleContent-MainContent">
+// ${aiUpdateTextArticleHtml}
+// </div>
+// <!--<a href="#" class="AiZoo20260401-CloseClickable">Close</a>-->
+// </div>
+// </div>
+// `;
+
+    // msgElement.querySelector('.AiZoo20260401-CloseClickable').addEventListener('click', (e) => {
+    //     console.log('Close click!');
+    //     msgElement.classList.remove('AiZoo20260401MsgArticleContainer-shown');
+    //     e.preventDefault();
+    //     return false;
+    // });
+
+    let titleEl = document.getElementById('id-AiZoo20260401-MsgArticleContent-Title');
+    document.body.addEventListener('mousemove', (e) => {
         let rect = titleEl.getBoundingClientRect();
         let x = 100 - ((e.clientX - rect.left) / rect.width) * 100;
         let y = 100 - ((e.clientY - rect.top) / rect.height) * 100;
         titleEl.style.backgroundPosition = `${x}% ${y}%`;
     });
 
-    let aEl = bannerElement.querySelector('a.AiZoo20260401-Banner-LearnMoreLink');
-    aEl.addEventListener('click', (e) => {
-        console.log('Showing full AI announcement message...');
-        msgElement.classList.add('AiZoo20260401MsgArticleContainer-shown');
-        e.preventDefault();
-        return false;
-    });
+    // document.body.appendChild(msgElement);
+}
 
-    document.body.appendChild(bannerElement);
-    document.body.appendChild(msgElement);
+function animateAiZoo20260401()
+{
+    if (window.location.pathname.endsWith("/__41ai")) {
+        animateAiZoo20260401_aipostpage();
+    } else {
+        animateAiZoo20260401_banner();
+    }
 }
 
 window.addEventListener('load', function() {
-    setTimeout(animateAiZoo20260401, 800);
+    setTimeout(animateAiZoo20260401, 500);
 });
